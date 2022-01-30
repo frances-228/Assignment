61b2794c8332fb98d55730e19a5f391424c92033
Frances Cui <frances.cui228@gmail.com>
2022-01-30 21:09:18 +1100

    draft on 30/01/2022

--- Assignment code.R
+++ Assignment code.R
@@ -6,8 +6,30 @@ library(readxl)
 library(dplyr)
 library(data.table)
 library(caret)
+library(DataExplorer)
 
 VarPathStore <- getwd()
 VarPathSeparator <- "/"
 train_tbl <- as.data.table(read.csv(file = paste(VarPathStore,"pml-training.csv", sep=VarPathSeparator)))
 test_tbl <- as.data.table(read.csv(file = paste(VarPathStore, "pml-testing.csv", sep = VarPathSeparator)))
+
+# glimpse(train_tbl)
+introduce(train_tbl)
+plot_intro(train_tbl)
+plot_missing(train_tbl)
+missing <- profile_missing(train_tbl)
+
+non_missing <- missing %>% filter(pct_missing<0.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
