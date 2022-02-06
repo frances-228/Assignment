58d1e127635cc19c8022615f8f56041d9823a42a
Frances Cui <frances.cui228@gmail.com>
2022-02-06 12:16:24 +1100

    Draft on 06/02/2022

--- Assignment code.R
+++ Assignment code.R
@@ -20,39 +20,39 @@ train_tbl <- setDT(train_tbl)
 glimpse(train_tbl)
 
 var_category <- c("kurtosis_roll_belt"
-                 ,"kurtosis_picth_belt"
-                 ,"kurtosis_yaw_belt"
-                 ,"skewness_roll_belt"
-                 ,"skewness_roll_belt.1"
-                 ,"skewness_yaw_belt"
-                 ,"max_yaw_belt"
-                 ,"min_yaw_belt"
-                 ,"amplitude_yaw_forearm"
-                 ,"kurtosis_roll_arm"
-                 ,"kurtosis_picth_arm"
-                 ,"kurtosis_yaw_arm"
-                 ,"skewness_roll_arm"
-                 ,"skewness_pitch_arm"
-                 ,"skewness_yaw_arm"
-                 ,"kurtosis_roll_dumbbell"
-                 ,"kurtosis_picth_dumbbell"
-                 ,"kurtosis_yaw_dumbbell"
-                 ,"skewness_roll_dumbbell"
-                 ,"skewness_pitch_dumbbell"
-                 ,"skewness_yaw_dumbbell"
-                 ,"max_yaw_dumbbell"
-                 ,"min_yaw_dumbbell"
-                 ,"amplitude_yaw_dumbbell"
-                 ,"kurtosis_roll_forearm"
-                 ,"kurtosis_picth_forearm"
-                 ,"kurtosis_yaw_forearm"
-                 ,"skewness_roll_forearm"
-                 ,"skewness_pitch_forearm"
-                 ,"skewness_yaw_forearm"
-                 ,"max_yaw_forearm"
-                 ,"min_yaw_forearm"
-                 ,"amplitude_yaw_belt"
-                 )
+                  ,"kurtosis_picth_belt"
+                  ,"kurtosis_yaw_belt"
+                  ,"skewness_roll_belt"
+                  ,"skewness_roll_belt.1"
+                  ,"skewness_yaw_belt"
+                  ,"max_yaw_belt"
+                  ,"min_yaw_belt"
+                  ,"amplitude_yaw_forearm"
+                  ,"kurtosis_roll_arm"
+                  ,"kurtosis_picth_arm"
+                  ,"kurtosis_yaw_arm"
+                  ,"skewness_roll_arm"
+                  ,"skewness_pitch_arm"
+                  ,"skewness_yaw_arm"
+                  ,"kurtosis_roll_dumbbell"
+                  ,"kurtosis_picth_dumbbell"
+                  ,"kurtosis_yaw_dumbbell"
+                  ,"skewness_roll_dumbbell"
+                  ,"skewness_pitch_dumbbell"
+                  ,"skewness_yaw_dumbbell"
+                  ,"max_yaw_dumbbell"
+                  ,"min_yaw_dumbbell"
+                  ,"amplitude_yaw_dumbbell"
+                  ,"kurtosis_roll_forearm"
+                  ,"kurtosis_picth_forearm"
+                  ,"kurtosis_yaw_forearm"
+                  ,"skewness_roll_forearm"
+                  ,"skewness_pitch_forearm"
+                  ,"skewness_yaw_forearm"
+                  ,"max_yaw_forearm"
+                  ,"min_yaw_forearm"
+                  ,"amplitude_yaw_belt"
+)
 
 for (var in var_category){
   var_class <- train_tbl %>% pull(var) %>% class()
@@ -78,18 +78,18 @@ train_sb <- train_tbl[, ..col_list]
 plot_histogram(train_sb)
 
 remove_col<- c("new_window"
-              ,"num_window"
-              ,"X"
-              ,"user_name"
-              ,"raw_timestamp_part_1"
-              ,"raw_timestamp_part_2"
-              ,"cvtd_timestamp"
-              ,"gyros_dumbbell_x"
-              ,"gyros_dumbbell_y"
-              ,"gyros_dumbbell_z"
-              ,"gyros_forearm_y"
-              ,"gyros_forearm_z"
-              )
+               ,"num_window"
+               ,"X"
+               ,"user_name"
+               ,"raw_timestamp_part_1"
+               ,"raw_timestamp_part_2"
+               ,"cvtd_timestamp"
+               ,"gyros_dumbbell_x"
+               ,"gyros_dumbbell_y"
+               ,"gyros_dumbbell_z"
+               ,"gyros_forearm_y"
+               ,"gyros_forearm_z"
+)
 
 train_sb_te <- train_sb[, -..remove_col]
 
@@ -126,13 +126,18 @@ trainPC <- cbind(trainPC,target)
 ##### ___B: Fit the model #####
 ##############################@
 train_sb_te$classe <- factor(train_sb_te$classe)
+trainPC$classe <- factor(trainPC$classe)
+
 mod_gbm <- train(classe~., method="gbm",data=trainPC,verbose=FALSE)
 
 mod_gbm_test <- train(classe~., method="gbm",data=train_sb_te,verbose=FALSE)
+mod_gbm_test_pred <- predict(mod_gbm_test,train_sb_te[,-c("classe")])
+mod_gbm_pred <- predict(mod_gbm_test,test_tbl)
 
 
+confusionMatrix(data=mod_gbm_test_pred,reference=trainPC$classe)
 
-
+summary(mod_gbm_test)
 
 
 
